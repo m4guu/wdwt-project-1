4f2f6802ff374355fde189565ce555e949ec5f31
pand4 <krystian.domza@gmail.com>
2023-11-24 22:47:28 +0100

    refactore: change all, optymalize code and add comments

--- main.py
+++ main.py
@@ -2,86 +2,60 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # Load the CSV file into a DataFrame
-df = pd.read_csv('data/data.csv')
-df[['TC2S3']].plot()
-plt.xlabel('Samples')
-plt.ylabel('Temperature [°C]')
-plt.title('Histogram for TC2 in series 3 before procssing')
-plt.savefig('images/before_data_processing.png')
-
+df = pd.read_csv('data/data.csv')  
 # Replace non-numeric values with NaN
-df = df.apply(pd.to_numeric, errors='coerce')
+df = df.apply(pd.to_numeric, errors='coerce')       
 
+labels = ['TC1S1', 'TC2S1', 'TC1S2', 'TC2S2', 'TC1S3', 'TC2S3']
+
+fig, axes = plt.subplots(nrows=3, ncols=2, figsize=(12, 10))
+axes = axes.flatten()
+
+for i, label in enumerate(labels):
+    df[[label]].plot(ax=axes[i])
+    axes[i].set_xlabel('Samples')
+    axes[i].set_ylabel('Temperature [°C]')
+    axes[i].set_title(f'Chart for {label}')
+
+plt.tight_layout()
+plt.savefig('images/fig_1.png')
+
+### Filtering TC2S3
+## 1st method
+# Replace values greater than 40 with the median for each column
+median_temp = df[df<40].median()
+df[df > 40] = median_temp.iloc[3]
+## 2nd method
+# Standard deviation
+std_TC2S3 = df[['TC2S3']].std().iloc[0]
+# Iterating through columns and eliminating errors
+for column in df.columns:
+    for i in range(1, len(df)):
+        diff = abs((df[column][i] - df[column][i-1]))
+        if diff >= std_TC2S3 * 2:
+            df.at[i, column] = df.at[i-1, column]
+
+
+### Fill NaN values
+# Count the number of NaN values in each column
+nan_count = df.isna().sum()
+# Calculate the procentage of missing values for TC1S1 and TC2S2
+percentage_missing_TC1S1 = df[['TC1S1']].isna().mean() * 100
+percentage_missing_TC2S2 = df[['TC2S2']].isna().mean() * 100
 # Forward/Backward fill NaN values (fill NaN with the previous/next non-NaN value in the column)
 df = df.ffill()
 df = df.bfill()
 
-# MEDIAN
-median_temp = df[df<40].median()
-# Replace values greater than 40 with the median for each column
-df[df > 40] = median_temp.iloc[3]
-df[['TC2S3']].plot()
-plt.xlabel('Samples')
-plt.ylabel('Temperature [°C]')
-plt.title('Histogram for TC2 in series 3 after procssing using 1st method')
-plt.savefig('images/after_data_processing_1.png')
+### STATISTICAL INDICATORS
+mean_values = df.mean()             # Wartość średnia arytmetyczna
+range_values = df.max() - df.min()  # Rozstęp
+mode_values = df.mode().iloc[0]     # Wartość modalna (dominanta)
+median_values = df.median()         # Mediana
+skewness_values = df.skew()         # Skośność
+kurtosis_values = df.kurtosis()     # Szczytowość (kurtoza)
+std_dev_values = df.std()           # Odchylenie standardowe
 
-# Iteracja po kolumnach i eliminacja błędów
-for column in df.columns:
-    for i in range(1, len(df)):
-        diff = abs((df[column][i] - df[column][i-1]))
-        if diff >= 0.2:
-            df.at[i, column] = df.at[i-1, column]
-
-
-df[['TC2S3']].plot()
-plt.xlabel('Samples')
-plt.ylabel('Temperature [°C]')
-plt.title('Histogram for TC2 in series 3 after procssing using 2nd method')
-plt.savefig('images/after_data_processing_2.png')
-# 1. Wartość średnia arytmetyczna
-mean_values = df.mean()
-
-# 2. Rozstęp
-range_values = df.max() - df.min()
-
-# 3. Wartość modalna (dominanta)
-mode_values = df.mode().iloc[0]
-
-# 4. Mediana
-median_values = df.median()
-
-# 5. Skośność
-skewness_values = df.skew()
-
-# 6. Szczytowość (kurtoza)
-kurtosis_values = df.kurtosis()
-
-# 7. Odchylenie standardowe
-std_dev_values = df.std()
-
-# Wydrukuj wyniki
-print("1. Wartość średnia arytmetyczna:")
-print(mean_values)
-
-print("\n2. Rozstęp:")
-print(range_values)
-
-print("\n3. Wartość modalna (dominanta):")
-print(mode_values)
-
-print("\n4. Mediana:")
-print(median_values)
-
-print("\n5. Skośność:")
-print(skewness_values)
-
-print("\n6. Szczytowość (kurtoza):")
-print(kurtosis_values)
-
-print("\n7. Odchylenie standardowe:")
-print(std_dev_values)
-
+###
 # Select columns for histograms
 columns_to_plot = [['TC1S1', 'TC2S1'], ['TC1S2', 'TC2S2'], ['TC1S3', 'TC2S3']]
 
@@ -98,21 +72,3 @@ for i, columns in enumerate(columns_to_plot):
 # Adjust layout
 plt.tight_layout()
 plt.savefig('images/histogramy_temperatur.png')
-
-df[['TC1S1', 'TC1S2', 'TC1S3']].plot(kind='hist', bins=40, edgecolor='black', linewidth=1.2, alpha=0.7)
-plt.xlabel('Temperature [°C]')
-plt.title('Histogram for TC1 in 3 series')
-plt.savefig('images/histogramy_temperatur_2.png')
-
-print
-df[['TC1S1']].plot()
-plt.savefig('images/line_chart_TC1S1')
-df[['TC2S1']].plot()
-plt.savefig('images/line_chart_TC2S1')
-
-window_size = 5
-TC2S1_smooth = df[['TC2S1']].rolling(window=window_size).mean()
-TC2S1_smooth.plot()
-plt.savefig('images/line_chart_TC2S1_smooth')
-
-
